c569b03e9711067449362297e7295084f8562dd6
kevinwasike <kelvinwasike007@gmail.com>
2023-05-03 21:02:25 +0000

    where users will find their school domain

---
+++ src/routes/schools/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { onMount } from "svelte";
+    let schools= []
+    onMount(async ()=>{
+        const schReq = await fetch("https://school-platform-api.onrender.com/schools")
+        schools = await schReq.json()
+    })
+    $:schools
+</script>
+search school
