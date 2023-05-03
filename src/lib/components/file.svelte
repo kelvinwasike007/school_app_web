c97ea5625e66c65da213bfcc0e3ef8d9d6086edf
kevinwasike <kelvinwasike007@gmail.com>
2023-05-03 20:57:01 +0000

    file input component

---
+++ src/lib/components/file.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let files, ph
+</script>
+<label for=""  class="label">
+    <span class="label-text">{ph}</span>
+</label>
+<input files={files} type="file" class="file-input file-input-bordered file-input-primary w-full max-w-xs"  on:input={e => files = e.target.files} placeholder={ph} />
