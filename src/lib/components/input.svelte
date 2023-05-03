f3f3270cc72d60844eb97c64be75a252a0062ac8
kevinwasike <kelvinwasike007@gmail.com>
2023-05-03 20:57:17 +0000

    text input component

---
+++ src/lib/components/input.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let value, ph, type
+</script>
+
+<label for=""  class="label">
+<span class="label-text">{ph}</span>
+</label>
+<input files={value} on:input={e => value = e.target.value} type={type} placeholder={ph} class="input input-bordered input-primary w-full max-w-xs" />
