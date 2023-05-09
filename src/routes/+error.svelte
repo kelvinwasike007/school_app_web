6faf9da35fa831ac03f4d2e3b423627dcd41a8dc
kevinwasike <kelvinwasike007@gmail.com>
2023-05-09 22:03:52 +0000

    custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<div data-theme="corporate" class="flex flex-col items-center justify-center items-center justify-center h-screen w-screen overflow-x-hidden " style="background-image: url(/bg.jpg);">
+    <div class="card bg-white shadow-lg shadow-gray-800">
+        <div class="card-body">
+            <div class="card-title">An Error Occured</div>
+            <div class="card-actions">
+                <a href="/" class="btn btn-primary btn-sm">GO TO HOME</a><a href="/report" class="btn btn-warning btn-sm">REPORT ISSUE</a>
+            </div>
+        </div>
+    </div>
+</div>
