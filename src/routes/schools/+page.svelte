fe20fe7a0ccce7f798bb5221ebe51adb72bf3015
kevinwasike <kelvinwasike007@gmail.com>
2023-05-09 22:05:51 +0000

    Search for yout school easly

--- src/routes/schools/+page.svelte
+++ src/routes/schools/+page.svelte
@@ -1,10 +1,32 @@
 <script>
     import { onMount } from "svelte";
-    let schools= []
+    import {browser} from '$app/environment'
+    let schools= [], school = "ext"
     onMount(async ()=>{
         const schReq = await fetch("https://school-platform-api.onrender.com/schools")
         schools = await schReq.json()
     })
     $:schools
+    $: school
+    $: if(school != "ext"){
+        
+        if(browser){
+            window.location.href=`/${school.schooldomain}/login`
+            localStorage.setItem('school', JSON.stringify(school))
+            console.log(school._id);
+        }
+    }
 </script>
-search school
+<div data-theme="corporate" class="flex flex-col items-center justify-center items-center justify-center h-screen w-screen overflow-x-hidden " style="background-image: url(/bg.jpg);">
+    <div class="card bg-white shadow-xl shadow-gray-800 w-96">
+        <div class="card-body w-full">
+            <div class="card-title justify-center ">SEARCH SCHOOL</div>
+            <select bind:value={school} class="select select-primary">
+                <option disabled selected class="p-4" value="ext">SEARCH SCHOOL</option>
+                {#each schools as school }
+                    <option value={school}>{school.schoolName}</option>
+                {/each}
+            </select>
+        </div>
+    </div>
+</div>
